2a0412503147893ba3d60869da4c3528ccfc81fa
WC <slava.sviat2023@gmail.com>
2024-02-12 21:55:56 +0300

    12.02.2024

---
+++ src/lesson/lesson23.java
@@ -0,0 +1,15 @@
+//package lesson;
+//
+//public class lesson23 {
+//    public static void main(String[] args) {
+//        Thread t =  new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println(i++);
+//            }
+//        });
+//    }
+//private static Runnable getRunnabeleTask(){
+//    }
+//}
+//
